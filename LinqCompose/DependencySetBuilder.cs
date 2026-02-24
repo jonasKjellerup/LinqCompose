a919ed7f1a592ba721c48d8c4388bfe0a3c703ec
Jonas Kjellerup <jmk@humanrisks.com>
2026-02-24 11:22:25 +0100

    Add base for DependencySetBuilder

---
+++ LinqCompose/DependencySetBuilder.cs
@@ -0,0 +1,34 @@
+using System.Reflection;
+
+namespace LinqTools;
+
+public class DependencySetBuilder
+{
+    private readonly HashSet<PropertyDependency> _dependencies = [];
+
+    public DependencySetBuilder AddProperty(PropertyInfo propertyInfo)
+    {
+        var dep = new PropertyDependency
+        {
+            Property = propertyInfo,
+            Dependencies = [],
+        };
+
+        if (_dependencies.TryGetValue(dep, out var value))
+        {
+            _dependencies.Add(value.Union(dep));
+        }
+        else
+        {
+            _dependencies.Add(dep);
+        }
+
+        return this;
+    }
+    
+    public HashSet<PropertyDependency> Result()
+    {
+        var set = new HashSet<PropertyDependency>(_dependencies);
+        return set;
+    }
+}
