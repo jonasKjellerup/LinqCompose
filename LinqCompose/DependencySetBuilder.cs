77d2eec2ed365918e1c567ce7039db227d473ae7
Jonas Mærsk Thye Kjellerup <jonas@kjellerup.dev>
2026-02-25 21:03:27 +0100

    Add support for nested properties in DependencySetBuilder

--- LinqCompose/DependencySetBuilder.cs
+++ LinqCompose/DependencySetBuilder.cs
@@ -1,36 +1,71 @@
+using System.Diagnostics;
 using System.Diagnostics.Contracts;
 using System.Reflection;
 
 namespace LinqCompose;
 
-public class DependencySetBuilder
+public class DependencySetBuilder(bool validatePath = false)
 {
     private readonly HashSet<PropertyDependency> _dependencies = [];
 
     public DependencySetBuilder AddProperty(PropertyInfo propertyInfo)
     {
-        var dep = new PropertyDependency
-        {
-            Property = propertyInfo,
-            Dependencies = [],
-        };
-
-        if (_dependencies.TryGetValue(dep, out var value))
-        {
-            _dependencies.Add(value.Union(dep));
-        }
-        else
-        {
-            _dependencies.Add(dep);
-        }
-
-        return this;
+        return AddPropertyPath(propertyInfo);
     }
     
+    /// <summary>
+    /// Adds a sequence of nested dependencies.
+    /// For this to generate a valid dependency tree, each specified property
+    /// must be a member of the previously given property.
+    /// If <see cref="validatePath"/> is set to `true` then failing to uphold this will throw an error.
+    /// </summary>
+    public DependencySetBuilder AddPropertyPath(params PropertyInfo[] properties)
+    {
+        if (properties.Length == 0)
+        {
+            return this;
+        }
+
+        Type? targetType = null;
+
+        var deps = _dependencies;
+        
+        foreach (var property in properties)
+        {
+            if (validatePath && targetType is not null)
+            {
+                ValidateDependency(targetType, property);
+                targetType = property.PropertyType;
+            }
+            
+            Debug.Assert(deps is not null);
+            if (deps.TryGetValue(new PropertyDependency { Property = property }, out var node))
+            {
+                deps = node.Dependencies;
+            }
+            else
+            {
+                deps.Add(new PropertyDependency { Property = property, Dependencies = [] });
+            }
+        }
+        
+        return this;
+    }
+
+    private static void ValidateDependency(Type target, PropertyInfo dependency)
+    {
+        if (dependency.DeclaringType?.IsAssignableFrom(target) is not true)
+        {
+            throw new InvalidOperationException($"");
+        }
+    }
+    
+    /// <summary>
+    /// Get a reference to the dependency tree.
+    /// </summary>
     [Pure]
     public HashSet<PropertyDependency> Result()
     {
-        var set = new HashSet<PropertyDependency>(_dependencies);
-        return set;
+        return _dependencies;
     }
 }
